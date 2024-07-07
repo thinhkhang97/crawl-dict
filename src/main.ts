b1fcf770601df2db5b5e5e0ad1157756e9d731c3
Khang Nguyễn <thinhkhang97@gmail.com>
2024-07-07 20:02:35 +0700

    complete crawling words

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { Runner } from './runner';
 import { parseArgs } from './utils';
 
-function main(args: string[]): void {
+async function main(args: string[]) {
   const params = parseArgs(args) as { index: string };
   const runner = new Runner();
-  runner.crawlWords(parseInt(params.index, 10));
+  const retries = 3;
+
+  let _error: Error | null = null;
+
+  for (let i = 0; i < retries; i++) {
+    try {
+      await runner.crawlWords(parseInt(params.index, 10));
+      break;
+    } catch (error) {
+      console.log('RETRYING', i + 1);
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      _error = error;
+    }
+  }
+
+  if (_error) {
+    throw _error;
+  }
 }
 
 main(process.argv.slice(1));
